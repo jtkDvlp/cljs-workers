2937ea7f896046f36a24411712ae830d2e50fbb2
JT Knabenschuh <jtkdevelopments@gmail.com>
2017-02-24 22:15:24 +0100

    Creating worker and worker pool

---
+++ src/cljs_workers/core.cljs
@@ -0,0 +1,27 @@
+(ns cljs-workers.core
+  (:require [cljs.core.async :refer [chan]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+(defn supported?
+  []
+  (-> js/Worker
+      undefined?
+      not))
+
+(defn create-worker
+  [script]
+  (js/Worker. script))
+
+(defn create-pool
+  ([]
+   (pool 5))
+
+  ([count]
+   (pool count "js/compiled/workers.js"))
+
+  ([count script]
+   (let [workers (chan count)]
+     (go
+       (dotimes [_ count]
+         (>! workers (create-worker script))))
+     workers)))
