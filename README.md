fd71788c0aa3df3d71305917d31516cb5efb9b8c
JT Knabenschuh <jtkdevelopments@gmail.com>
2017-02-24 18:11:39 +0100

    Minimal project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web workers for clojurescript
+
+...will follow...
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cljs-workers "1.0.0"
+  :description "A clojurescript lib for performing async tasks via web workers"
+  :url "https://github.com/jtkDvlp/cljs-workers"
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.229"]])
