cb4e6fac972d568cb4519d9d7dd580a568845848
JT Knabenschuh <jtkdevelopments@gmail.com>
2017-02-25 02:29:17 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-workers "1.0.0"
+(defproject cljs-workers "1.0.1-alpha"
   :description "A clojurescript lib for performing async tasks via web workers"
   :url "https://github.com/jtkDvlp/cljs-workers"
   :license {:name "MIT"}
