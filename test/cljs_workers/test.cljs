1ad67177522ce97a3774a989b22ee15f65f6a637
JT Knabenschuh <jtkdevelopments@gmail.com>
2017-03-01 22:38:45 +0100

    Test with better print fun

--- test/cljs_workers/test.cljs
+++ test/cljs_workers/test.cljs
@@ -4,11 +4,12 @@
 
 (defn app
   []
-  (let [worker-pool (main/create-pool 2 "js/worker/worker.js")]
-    (main/do-with-pool! worker-pool {:handler :mirror, :arguments {:a "Hallo" :b "Welt" :c 10}} #(.debug js/console %))
-    (main/do-with-pool! worker-pool {:handler :mirror, :arguments {:a "Hallo" :b "Welt" :c 10 :d (js/ArrayBuffer. 10) :transfer [:d]} :transfer [:d]} #(.debug js/console %))
-    (main/do-with-pool! worker-pool {:handler :mirror, :arguments {:a "Hallo" :b "Welt" :c 10 :d (js/ArrayBuffer. 10) :transfer [:d]} :transfer [:c]} #(.debug js/console %))
-    (main/do-with-pool! worker-pool {:handler :mirror, :arguments {:a "Hallo" :b "Welt" :c 10 :d (js/ArrayBuffer. 10) :transfer [:c]} :transfer [:d]} #(.debug js/console %))))
+  (let [worker-pool (main/create-pool 2 "js/worker/worker.js")
+        print-result #(.debug js/console (str (:state %)) %)]
+    (main/do-with-pool! worker-pool {:handler :mirror, :arguments {:a "Hallo" :b "Welt" :c 10}} print-result)
+    (main/do-with-pool! worker-pool {:handler :mirror, :arguments {:a "Hallo" :b "Welt" :c 10 :d (js/ArrayBuffer. 10) :transfer [:d]} :transfer [:d]} print-result)
+    (main/do-with-pool! worker-pool {:handler :mirror, :arguments {:a "Hallo" :b "Welt" :c 10 :d (js/ArrayBuffer. 10) :transfer [:d]} :transfer [:c]} print-result)
+    (main/do-with-pool! worker-pool {:handler :mirror, :arguments {:a "Hallo" :b "Welt" :c 10 :d (js/ArrayBuffer. 10) :transfer [:c]} :transfer [:d]} print-result)))
 
 (defn worker
   []
