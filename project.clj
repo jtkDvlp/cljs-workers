2f67c55b58326b8bd0bcc0925b2b3f9b3abba4b0
Julian Knabenschuh <knabenschuh@doctronic.de>
2020-08-23 15:24:14 +0200

    Release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-workers "1.1.2-alpha"
+(defproject cljs-workers "1.1.2"
   :description "A clojurescript lib for performing async tasks via web workers"
   :url "https://github.com/jtkDvlp/cljs-workers"
   :license {:name "MIT"}
